9e2991a05bcf341091955c2617962aa53ae64b62
khoinh1 <khoinh1@forlife.vn>
2025-01-23 14:02:25 +0700

    add logs

--- src/app.ts
+++ src/app.ts
@@ -42,9 +42,34 @@ export class App {
     this.app.use(new CustomErrorHandler().error);
   }
   private startApi() {
-    console.log("Start api: ", this.port);
+    console.log("Start api with configs: ", JSON.stringify(appConfig, null, 2));
     this.httpServer.listen(this.port, () => {
       console.log(`🚀 Server Storage API started at http://localhost:${this.port}\n🚨️ Environment: ${process.env.NODE_ENV}`);
+
+      const listRoutes = (app) => {
+        const routes = [];
+        app._router.stack.forEach((middleware) => {
+          if (middleware.route) {
+            // Routes registered directly on the app
+            routes.push({
+              method: Object.keys(middleware.route.methods)[0].toUpperCase(),
+              path: middleware.route.path,
+            });
+          } else if (middleware.name === "router") {
+            // Routes added via router
+            middleware.handle.stack.forEach((nestedMiddleware) => {
+              if (nestedMiddleware.route) {
+                routes.push({
+                  method: Object.keys(nestedMiddleware.route.methods)[0].toUpperCase(),
+                  path: nestedMiddleware.route.path,
+                });
+              }
+            });
+          }
+        });
+        return routes;
+      };
+      console.log(listRoutes(this.app));
     });
   }
 
